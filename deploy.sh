3f88263e83ef154a6fde74a73f20e4b19d4a6795
Astami <astami@shipperstack.com>
2020-04-27 13:12:57 +0530

    NO_T fixing parameters in deploy.
    Signed-off-by: Astami <astami@shipperstack.com>

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,10 @@
 #
 [[ -z "$CLOUDANT_URL" ]] && { echo "Error: CLOUDANT_URL environment variable not set"; exit 1; }
 [[ -z "$CLOUDANT_DATABASE" ]] && { echo "Error: CLOUDANT_DATABASE environment variable not set"; exit 1; }
+[[ -z "$APPID_URL" ]] && { echo "Error: APPID_URL environment variable not set"; exit 1; }
 
 if which wskdeploy >/dev/null; then
-    wskdeploy -m manifest.yml --param "services.cloudant.url" "$CLOUDANT_URL" --param "services.cloudant.database" "$CLOUDANT_DATABASE"
+    wskdeploy -m manifest.yml --param "services.cloudant.url" "$CLOUDANT_URL" --param "services.cloudant.database" "$CLOUDANT_DATABASE" --param "services.appid.url" "$APPID_URL"
 else
     echo "wskdeploy not found on system path"
 fi
